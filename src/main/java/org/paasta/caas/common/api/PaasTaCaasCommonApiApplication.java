996cacb1ac70e6c09c959b02131b04f3fc0941fe
CissC2 <d3g3nyo@gmail.com>
2018-07-31 15:40:59 +0900

    Caas Common API frame Commit

---
+++ src/main/java/org/paasta/caas/common/api/PaasTaCaasCommonApiApplication.java
@@ -0,0 +1,12 @@
+package org.paasta.caas.common.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaasTaCaasCommonApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PaasTaCaasCommonApiApplication.class, args);
+	}
+}
