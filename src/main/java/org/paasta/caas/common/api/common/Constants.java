c63fed5af2ae24f8034fd9edb108e61c2d0ac070
rex <rexx4314@gmail.com>
2018-08-07 14:09:34 +0900

    add rest template config, service and property service

--- src/main/java/org/paasta/caas/common/api/common/Constants.java
+++ src/main/java/org/paasta/caas/common/api/common/Constants.java
@@ -12,6 +12,7 @@ public class Constants {
     public static final String RESULT_STATUS_SUCCESS = "SUCCESS";
     public static final String STRING_DATE_TYPE = "yyyy-MM-dd HH:mm:ss";
     public static final String STRING_TIME_ZONE_ID = "Asia/Seoul";
+    public static final String TARGET_CAAS_API = "caasApi";
 
     private Constants() {
         throw new IllegalStateException();
